61b2a8fcffcc1db67287f4ad77b16d39cfa5ba7c
Alex Manzi <alex.manzi22@gmail.com>
2022-01-19 22:47:44 -0500

    Most known bugs fixed

--- script.js
+++ script.js
@@ -3,12 +3,8 @@ window.addEventListener('scroll', function(){
     header.classList.toggle("sticky", window.scrollY > 0 )
     });
 
-function addEventListener() {
-    const projects = document.getElementById('projects');
-
+const projects = document.getElementById('projects');
 function clickAlert() {
   alert("Whoops, there doesn't seem to be anything here yet! Check back soon!");
 }
-
 projects.addEventListener('click', clickAlert);
-}
